05f17a5c3f92c1b8fb9ea220e1cf3cbc98edd90f
Justin G <galvezjustin@hotmail.com>
2022-06-29 15:11:17 -0700

    Began weather app. Figure out API forecast call

---
+++ public/script.js
@@ -0,0 +1,7 @@
+const searchElement = document.querySelector('[data-city-search]')
+const searchBox = new google.maps.places.SearchBox(searchElement)
+searchBox.addListener('places_changed', () => {
+    const place = searchBox.getPlaces()[0]
+    if(place == null) return
+    const current = 
+})
